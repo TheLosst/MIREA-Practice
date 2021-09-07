6c1f0ba817826c029c2c5d1cab49833507849ed5
TheL0st <70810051+TheL0st@users.noreply.github.com>
2021-09-07 18:41:23 +0300

    123

--- src/ru/mirea/The_Losst/pr3/Shape/Test_All.java
+++ src/ru/mirea/The_Losst/pr3/Shape/Test_All.java
@@ -1,10 +1,11 @@
 package ru.mirea.The_Losst.pr3.Shape;
 
 public class Test_All {
-    void Circle() {
+    static void Circle() {
         Circle krug = new Circle("Алый",true,15.3);
         System.out.println("Площадь круга: " + krug.getArea());
         System.out.println("Периметр круга: " + krug.getPerimeter());
+        System.out.println(krug);
     }
     void Rectagle(){
 
@@ -12,6 +13,6 @@ public class Test_All {
 
 
     public static void main(String[] args) {
-
+        Circle();
     }
 }
